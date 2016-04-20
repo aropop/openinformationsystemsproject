540a7df0d2ad328505c4752faeb7c2d891d6f39b
Aropop <aropop@gmail.com>
2016-04-20 18:50:21 +0200

    changes indicated by the professor

--- dbcreatetable.sql
+++ dbcreatetable.sql
@@ -1,12 +1,12 @@
-CREATE TABLE  IF NOT EXISTS `persons` (
-person_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(20) NOT NULL,
-length INTEGER(3) NOT NULL,
-gender VARCHAR(1) NOT NULL, /* 'm' for male, 'f' for female*/
-birthdate DATE NOT NULL,
-excercise_level INTEGER(2) NOT NULL, /* 1 low, 2 average, 3 high */
-email VARCHAR(50) NOT NULL, /* Possibly more emails in the future */
-CONSTRAINT uniquePerson UNIQUE (email) /* No 2 users with the same email */
+CREATE TABLE  IF NOT EXISTS `Person` (
+    person_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(20) NOT NULL,
+    length INTEGER(3) NOT NULL,
+    gender VARCHAR(1) NOT NULL, /* 'm' for male, 'f' for female*/
+    birthdate DATE NOT NULL,
+    excercise_level INTEGER(2) NOT NULL, /* 1 low, 2 average, 3 high */
+    email VARCHAR(50) NOT NULL, /* Possibly more emails in the future */
+    CONSTRAINT uniquePerson UNIQUE (email) /* No 2 users with the same email */
 ) ENGINE=INNODB;
 
 CREATE TABLE IF NOT EXISTS `looses_weight` (
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS `looses_weight` (
     start_date DATE,
     weight_to_lose INTEGER NOT NULL,
     CONSTRAINT looses_pk PRIMARY KEY (`person_id`, `start_date`, `period`),
-    FOREIGN KEY (person_id) REFERENCES `persons`(person_id) ON DELETE CASCADE
+    FOREIGN KEY (person_id) REFERENCES `Person`(person_id) ON DELETE CASCADE
 ) ENGINE=INNODB;
 
 CREATE TABLE IF NOT EXISTS `person_weighs` (
@@ -23,32 +23,33 @@ CREATE TABLE IF NOT EXISTS `person_weighs` (
     on_date DATE,
     weight INTEGER NOT NULL,
     CONSTRAINT weighs_pk PRIMARY KEY (`person_id`, `on_date`),
-    FOREIGN KEY (person_id) REFERENCES `persons`(person_id) ON DELETE CASCADE
+    FOREIGN KEY (person_id) REFERENCES `Person`(person_id) ON DELETE CASCADE
 ) ENGINE=INNODB;
 
-CREATE TABLE IF NOT EXISTS `recipes` (
+CREATE TABLE IF NOT EXISTS `Recipe` (
     recipe_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(20) NOT NULL,
     calories INTEGER NOT NULL
 ) ENGINE=INNODB;
 
-CREATE TABLE IF NOT EXISTS `ingredients` (
+CREATE TABLE IF NOT EXISTS `Ingredient` (
     ingredient_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(20) NOT NULL,
     calories INTEGER NOT NULL
 ) ENGINE=INNODB;
 
-CREATE TABLE IF NOT EXISTS `tags` (
+CREATE TABLE IF NOT EXISTS `Tag` (
     tag_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(20) NOT NULL
+    name VARCHAR(20) NOT NULL,
+    CONSTRAINT uniqueTagName UNIQUE (name)
 ) ENGINE=INNODB;
 
 CREATE TABLE IF NOT EXISTS `recipe_tag` (
     recipe_id INTEGER UNSIGNED,
     tag_id INTEGER UNSIGNED,
     CONSTRAINT recipe_tag_pk PRIMARY KEY (`recipe_id`, `tag_id`),
-    FOREIGN KEY (recipe_id) REFERENCES `recipes`(recipe_id) ON DELETE CASCADE,
-    FOREIGN KEY (tag_id) REFERENCES `tags`(tag_id) ON DELETE CASCADE
+    FOREIGN KEY (recipe_id) REFERENCES `Recipe` (recipe_id) ON DELETE CASCADE,
+    FOREIGN KEY (tag_id) REFERENCES `Tag`(tag_id) ON DELETE CASCADE
 ) ENGINE=INNODB;
 
 CREATE TABLE IF NOT EXISTS `recipe_ingredient` (
@@ -56,8 +57,8 @@ CREATE TABLE IF NOT EXISTS `recipe_ingredient` (
     ingredient_id INTEGER UNSIGNED,
     amount INTEGER NOT NULL,
     CONSTRAINT recipe_ingredient_pk PRIMARY KEY (`recipe_id`, `ingredient_id`),
-    FOREIGN KEY (recipe_id) REFERENCES `recipes`(recipe_id) ON DELETE CASCADE,
-    FOREIGN KEY (ingredient_id) REFERENCES `ingredients`(ingredient_id) ON DELETE CASCADE
+    FOREIGN KEY (recipe_id) REFERENCES `Recipe`(recipe_id) ON DELETE CASCADE,
+    FOREIGN KEY (ingredient_id) REFERENCES `Ingredient`(ingredient_id) ON DELETE CASCADE
 ) ENGINE=INNODB;
 
 CREATE TABLE IF NOT EXISTS `recipe_recipe` (
@@ -65,6 +66,6 @@ CREATE TABLE IF NOT EXISTS `recipe_recipe` (
     recipe_having_id INTEGER UNSIGNED,
     amount INTEGER NOT NULL,
     CONSTRAINT recipe_recipe_pk PRIMARY KEY (`recipe_part_id`, `recipe_having_id`),
-    FOREIGN KEY (recipe_part_id) REFERENCES `recipes`(recipe_id) ON DELETE CASCADE,
-    FOREIGN KEY (recipe_having_id) REFERENCES `recipes`(recipe_id) ON DELETE CASCADE
+    FOREIGN KEY (recipe_part_id) REFERENCES `Recipe`(recipe_id) ON DELETE CASCADE,
+    FOREIGN KEY (recipe_having_id) REFERENCES `Recipe`(recipe_id) ON DELETE CASCADE
 ) ENGINE=INNODB;
